505e02a5968c2e5c9d5fb47283e50fc5e7f6ee91
Alexandros Lotsos <alexlo@bu.edu>
2019-11-11 23:28:30 -0500

    added more passthroughcopy rules

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
     // Output directory: _site
   
+    eleventyConfig.addPassthroughCopy("static/images/");
     eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("_includes/assets/");
   };
